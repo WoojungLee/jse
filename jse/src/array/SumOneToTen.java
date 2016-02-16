12b46ab1456e6b0f34f628a720b829c9a26e4a81
user <user@user-PC>
2016-02-16 10:00:15 +0900

    ..

---
+++ jse/src/array/SumOneToTen.java
@@ -0,0 +1,16 @@
+package array;
+
+public class SumOneToTen {
+	public static void main(String[] args) {
+		int[] arr; //배열의 선언
+		arr = new int[3];
+		//arr = 3
+		int[] a = new int[10];
+		int b = 1;
+		a[0] = 1;
+		
+		
+		
+		
+	}
+}
